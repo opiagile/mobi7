12a2c41d88de5913ccf4b2fec72bc66e225667b5
Francisco Araujo <francisco@opiagile.com>
2021-09-08 22:50:59 -0300

    Add Api Gateway

---
+++ PosicaoAppApiGateway/src/main/java/br/com/mobi7/posicaoapp/api/gateway/PosicaoAppApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.mobi7.posicaoapp.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosicaoAppApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosicaoAppApiGatewayApplication.class, args);
+	}
+
+}
---
+++ PosicaoAppApiGateway/src/test/java/br/com/mobi7/posicaoapp/api/gateway/PosicaoAppApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mobi7.posicaoapp.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosicaoAppApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
